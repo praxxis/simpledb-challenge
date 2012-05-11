6e449bacf50bbc1637db530b99253b8bb60ee2c4
Lindsey Smith <lindsey@phparadox.com>
2012-05-10 21:14:54 -0700

    transactions (sans equalto)

--- db.py
+++ db.py
@@ -4,15 +4,25 @@ import string
 class Database(object):
 
 	def __init__(self):
-		self.reset()
+		self._reset()
+		self._reset_tx()
 
-	def reset(self):
+	def _reset(self):
 		self.values = {}
 		self.values_index = defaultdict(dict)
+
+	def _reset_tx(self):
+		self.tx_level = 0
+
+		# holds the values of self.values as they existed *before* they were changed in the
+		# current transaction (if they were changed at all)
+		self.tx_values = defaultdict(dict)
 
 	def set(self, name, value):
 		if name in self.values:
 			existing_value = self.values[name]
+
+			self.tx_values[self.tx_level][name] = existing_value
 
 			if existing_value in self.values_index:
 				del self.values_index[existing_value][name]
@@ -22,7 +32,7 @@ class Database(object):
 		self.values_index[value][name] = True
 		
 	def get(self, name):
-		if not name in self.values:
+		if not name in self.values or not self.values[name]:
 			return 'NULL'
 
 		return self.values[name]
@@ -33,15 +43,34 @@ class Database(object):
 			if self.values[name] in self.values_index:
 				del self.values_index[self.values[name]][name]
 
+			self.tx_values[self.tx_level][name] = self.values[name]
+
 			del self.values[name]
 
-		
 	def equalto(self, value):
 		if not value in self.values_index or not self.values_index[value]:
 			return 'NONE'
 
 		# iterkeys vs keys?
 		return ' '.join(sorted(self.values_index[value].keys()))
+
+	def begin(self):
+		self.tx_level += 1
+
+	def rollback(self):
+		if self.tx_level == 0:
+			return 'INVALID ROLLBACK'
+
+		if self.tx_values[self.tx_level]:
+			self.values.update(self.tx_values[self.tx_level])
+
+		self.tx_level -= 1
+
+
+	def commit(self):
+		# as we're applying all changes to the canonical value dict, all we need to do to commit
+		# is throw away all open transaction data
+		self._reset_tx()
 
 
 class LineInput(object):
--- test_db.py
+++ test_db.py
@@ -89,6 +89,64 @@ class TestDatabase(unittest.TestCase):
 		self.db.set('b', 30)
 
 		self.assertEqual(self.db.equalto(10), 'NONE')
+
+	def test_tx_commit(self):
+		self.db.set('a', 10)
+
+		self.db.begin()
+
+		self.assertEqual(self.db.get('a'), 10)
+
+		self.db.set('a', 20)
+
+		self.assertEqual(self.db.get('a'), 20)
+
+		self.db.commit()
+
+		self.assertEqual(self.db.get('a'), 20)
+
+	def test_tx_full_commit(self):
+
+		# test that commit commits *all* open transactions
+		self.db.begin()
+		self.db.set('a', 30)
+		self.db.begin()
+		self.db.set('a', 40)
+		self.db.commit()
+		self.assertEqual(self.db.get('a'), 40)
+		self.assertEqual(self.db.rollback(), 'INVALID ROLLBACK')
+
+	def test_tx_rollback(self):
+
+		self.db.set('a', 10)
+
+		self.db.begin()
+
+		self.assertEqual(self.db.get('a'), 10)
+
+		self.db.set('a', 20)
+
+		self.assertEqual(self.db.get('a'), 20)
+
+		self.db.rollback()
+
+		self.assertEqual(self.db.get('a'), 10)
+
+	def test_tx_invalid_rollback(self):
+		self.assertEqual(self.db.rollback(), 'INVALID ROLLBACK')
+
+	def test_tx_begin_rollback_commit(self):
+		self.db.set('a', 50)
+		self.db.begin()
+		self.assertEqual(self.db.get('a'), 50)
+		self.db.set('a', 60)
+		self.db.begin()
+		self.db.unset('a')
+		self.assertEqual(self.db.get('a'), 'NULL')
+		self.db.rollback()
+		self.assertEqual(self.db.get('a'), 60)
+		self.db.commit()
+		self.assertEqual(self.db.get('a'), 60)
 		
 if __name__ == '__main__':
 	unittest.main()
